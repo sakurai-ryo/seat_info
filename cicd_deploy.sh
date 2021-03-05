a46393baccecc43fe4e46df9f877be407a2c28f8
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-03-05 17:11:09 +0900

    update:#9 シェルの変数を大文字に

--- cicd_deploy.sh
+++ cicd_deploy.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-function usage {
+function usage() {
   cat <<EOM
   -s          Stage
   -h          Show Usage
@@ -10,22 +10,21 @@ EOM
   exit 2
 }
 
-stage=""
+Stage=""
 
-while getopts s:h: OPT
-do
+while getopts s:h: OPT; do
   case $OPT in
-    "s" ) stage=$OPTARG;;
-    '-h'|'--help'|* ) usage;;
+  "s") Stage=$OPTARG ;;
+  '-h' | '--help' | *) usage ;;
   esac
 done
 
-echo "DeployStage: ${stage}"
+echo "DeployStage: ${Stage}"
 
-# CHANGESET_OPTION="--no-execute-changeset"
+#  CHANGESET_OPTION="--no-execute-changeset"
 
 # # 引数に'dploy'文字列がある場合のみデプロイ
-# if [ $# = 1 ] && [ $1 = "deploy" ]; then
+#  if [ $# = 1 ] && [ $1 = "deploy" ]; then
 #   echo "deploy mode"
 #   CHANGESET_OPTION=""
 # fi
@@ -33,24 +32,22 @@ echo "DeployStage: ${stage}"
 # CFN_TEMPLATE=template.yml
 # CFN_STACK_NAME=SeatInfo
 
-
 # TODO: Deploy時のパラメーターは環境変数とかにする
 
 echo "--------- CodeStar Stack ----------"
 # CodeStar Stack
 aws cloudformation deploy \
-    --stack-name "${stage}-SeatInfo-codeStarConnection" \
-    --template-file aws/codeStarConnection/template.yml \
-    --parameter-overrides \
-        Stage=${stage}
-
+  --stack-name "${Stage}-SeatInfo-codeStarConnection" \
+  --template-file aws/codeStarConnection/template.yml \
+  --parameter-overrides \
+  Stage=${stage}
 
 echo "--------- CodePipeline Stack ----------"
 # CodePipeline Stack
 aws cloudformation deploy \
-    --stack-name "${stage}-SeatInfo-codePipeline" \
-    --template-file aws/codePipeline/template.yml \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides \
-        Stage=${stage} \
-        BranchName=fix/codebuild#7
+  --stack-name "${Stage}-SeatInfo-codePipeline" \
+  --template-file aws/codePipeline/template.yml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+  Stage=${Stage} \
+  BranchName=fix/codebuild#7
