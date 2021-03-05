75f5db4085094b31806c6a56eb332171f67c4408
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-03-05 21:34:47 +0900

    update:#1 open closeでテンプレートを分岐

--- app/main.go
+++ app/main.go
@@ -12,25 +12,21 @@ import (
 
 func main() {
 	engine := gin.Default()
-	engine.LoadHTMLGlob("templates/*.html")
-	today := time.Date(2021, 2, 14, 12, 5, 6, 0, time.Local)
+	// today := time.Date(2021, 2, 14, 12, 5, 6, 0, time.Local)
+	engine.LoadHTMLGlob("./templates/**/*")
+	today := time.Now()
 	engine.GET("/", func(c *gin.Context) {
-		logging(c)
+		// logging(c)
 
 		var path string
 		if shared.IsOpen(today) {
-			path = "open/index.html"
+			path = "open/open.html"
 		} else {
-			path = "close/index.html"
+			path = "close/close.html"
 		}
-		log.Print(path)
 		c.HTML(http.StatusOK, path, gin.H{
-			// htmlに渡す変数を定義
 			"message": "hello World",
 		})
-		// c.JSON(http.StatusOK, gin.H{
-		// 	"message": "hello world",
-		// })
 	})
 	engine.Run(":8080")
 }
