65636620652aecebbd1217288484c858208444b4
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-03-05 21:12:19 +0900

    update:#1 logging処理を関数化

--- app/main.go
+++ app/main.go
@@ -4,47 +4,53 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"seat_info/shared"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
-	// engine.LoadHTMLGlob("templates/*.html")
-	// today := time.Date(2021, 2, 14, 12, 5, 6, 0, time.Local)
+	engine.LoadHTMLGlob("templates/*.html")
+	today := time.Date(2021, 2, 14, 12, 5, 6, 0, time.Local)
 	engine.GET("/", func(c *gin.Context) {
-		if res, err := json.Marshal(&c.Request.Header); err != nil {
-			log.Print(err)
-		} else {
-			log.Print(string(res))
-		}
+		logging(c)
 
-		if res, err := json.Marshal(&c.Request.URL); err != nil {
-			log.Print(err)
+		var path string
+		if shared.IsOpen(today) {
+			path = "open/index.html"
 		} else {
-			log.Print(string(res))
+			path = "close/index.html"
 		}
-
-		if res, err := json.Marshal(&c.Request.Body); err != nil {
-			log.Print(err)
-		} else {
-			log.Print(string(res))
-		}
-
-		// var path string
-		// if shared.IsOpen(today) {
-		// 	path = "open/index.html"
-		// } else {
-		// 	path = "close/index.html"
-		// }
-		// log.Print(path)
-		// c.HTML(http.StatusOK, path, gin.H{
-		// 	// htmlに渡す変数を定義
-		// 	"message": "hello World",
-		// })
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
+		log.Print(path)
+		c.HTML(http.StatusOK, path, gin.H{
+			// htmlに渡す変数を定義
+			"message": "hello World",
 		})
+		// c.JSON(http.StatusOK, gin.H{
+		// 	"message": "hello world",
+		// })
 	})
-	engine.Run(":80")
+	engine.Run(":8080")
+}
+
+func logging(c *gin.Context) {
+	if res, err := json.Marshal(&c.Request.Header); err != nil {
+		log.Print(err)
+	} else {
+		log.Print(string(res))
+	}
+
+	if res, err := json.Marshal(&c.Request.URL); err != nil {
+		log.Print(err)
+	} else {
+		log.Print(string(res))
+	}
+
+	if res, err := json.Marshal(&c.Request.Body); err != nil {
+		log.Print(err)
+	} else {
+		log.Print(string(res))
+	}
 }
