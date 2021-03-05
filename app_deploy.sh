3a3534cca8b6e081e10840760a7cb60ae481fbb2
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-03-05 17:10:07 +0900

    update:#9 app_deploy.shに引数を追加

--- app_deploy.sh
+++ app_deploy.sh
@@ -2,32 +2,39 @@
 
 set -eu
 
-function usage {
+function usage() {
   cat <<EOM
   -s          Stage
+  -t          TargetStack
   -h          Show Usage
 EOM
   exit 2
 }
 
 Stage=""
+Target=""
 
-while getopts s:h: OPT
-do
+while getopts s:t:h: OPT; do
   case $OPT in
-    "s" ) Stage=$OPTARG;;
-    '-h'|'--help'|* ) usage;;
+  "s") Stage=$OPTARG ;;
+  "t") Target=$OPTARG ;;
+  '-h' | '--help' | *) usage ;;
   esac
 done
 
 echo "DeployStage: ${Stage}"
+echo "DeployStack: ${Target}"
 
 ProjectName="SeatInfo"
 
-# CHANGESET_OPTION="--no-execute-changeset"
+if [ ${Target} == "" ]; then
+  echo "TargetStack(-t)を指定してください"
+  exit 1
+fi
+#  CHANGESET_OPTION="--no-execute-changeset"
 
 # # 引数に'dploy'文字列がある場合のみデプロイ
-# if [ $# = 1 ] && [ $1 = "deploy" ]; then
+#  if [ $# = 1 ] && [ $1 = "deploy" ]; then
 #   echo "deploy mode"
 #   CHANGESET_OPTION=""
 # fi
@@ -35,53 +42,60 @@ ProjectName="SeatInfo"
 # CFN_TEMPLATE=template.yml
 # CFN_STACK_NAME=ProjectName
 
-
 # TODO: Deploy時のパラメーターは環境変数とかにする
 
-echo "---------- Network Stack ----------"
-# Network Stack
-aws cloudformation deploy \
+if [ ${Target} = "all" ] || [ ${Target} = "network" ]; then
+  echo "---------- Network Stack ----------"
+  # Network Stack
+  aws cloudformation deploy \
     --stack-name "${Stage}-${ProjectName}-network" \
     --template-file aws/network/template.yml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        AZa=ap-northeast-1a \
-        AZb=ap-northeast-1c \
-        VpcCidrBlock=192.168.0.0/16 \
-        MainPublicSubnetCidrBlock=192.168.1.0/24 \
-        SubPublicSubnetCidrBlock=192.168.2.0/24 \
-        Stage=${Stage}
+    AZa=ap-northeast-1a \
+    AZb=ap-northeast-1c \
+    VpcCidrBlock=192.168.0.0/16 \
+    MainPublicSubnetCidrBlock=192.168.1.0/24 \
+    SubPublicSubnetCidrBlock=192.168.2.0/24 \
+    Stage=${Stage}
+fi
 
-echo "---------- WAF Stack ----------"
-# WAF Stack
-aws cloudformation deploy \
+if [ ${Target} = "all" ] || [ ${Target} = "waf" ]; then
+  echo "---------- WAF Stack ----------"
+  # WAF Stack
+  aws cloudformation deploy \
     --stack-name "${Stage}-${ProjectName}-waf" \
     --template-file aws/waf/template.yml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        Stage=${Stage}
+    Stage=${Stage}
+fi
 
-echo "---------- SecretManager Stack ----------"
-# SecretManager Stack
-aws cloudformation deploy \
+if [ ${Target} = "all" ] || [ ${Target} = "secret" ]; then
+  echo "---------- SecretManager Stack ----------"
+  # SecretManager Stack
+  aws cloudformation deploy \
     --stack-name "${Stage}-${ProjectName}-secret-manager" \
     --template-file aws/secretManager/template.yml \
     --parameter-overrides \
-      Stage=${Stage}
-
-echo "---------- Fargate Stack ----------"
-DesiredCount=0
-ExistService=$(aws ecs list-task-definitions --region ap-northeast-1 | jq '.taskDefinitionArns[] | select(contains("SeatInfo"))')
-if [ -n "$ExistService" ]; then
-  DesiredCount=2
+    Stage=${Stage}
 fi
-echo "DesiredCount: ${DesiredCount}"
-# Fargate Stack
-aws cloudformation deploy \
+
+if [ ${Target} = "all" ] || [ ${Target} = "fargate" ]; then
+  echo "---------- Fargate Stack ----------"
+  DesiredCount=0
+  ExistService=$(aws ecs list-task-definitions --region ap-northeast-1 | jq '.taskDefinitionArns[] | select(contains("SeatInfo"))')
+  if [ -n "$ExistService" ]; then
+    DesiredCount=2
+  fi
+  echo "DesiredCount: ${DesiredCount}"
+  # Fargate Stack
+  aws cloudformation deploy \
     --stack-name "${Stage}-${ProjectName}-fargate" \
     --template-file aws/fargate/template.yml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-      ProjectName=${ProjectName} \
-      DesiredCount=${DesiredCount} \
-      Stage=${Stage}
+    ProjectName=${ProjectName} \
+    DesiredCount=${DesiredCount} \
+    Stage=${Stage}
+fi
